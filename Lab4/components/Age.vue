b994c1c0317bae4d4c1c1704971c40ecf5306030
Vinhrox2034 <vinhlcts01565@gmail.com>
2026-01-23 12:06:47 +0700

    Add files via upload

---
+++ Lab4/components/Age.vue
@@ -0,0 +1,13 @@
+<script setup>
+    import { ref,computed } from 'vue';
+const year = ref(null);
+const age = computed(()=>{
+    if(!year.value) return null;
+    const currentYear = new Date().getDate()
+    return currentYear
+});
+</script>
+<template>
+    <h1>Your age {{ age }}</h1>
+    <input v-model ="year" placeholder="enter your BR"/>
+</template>
---
+++ Lab4/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>Chào mừng đến với VueJS</h1>
+</template>
+
+<style>
+h1 {
+  color: red;
+}
+</style>
+
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
