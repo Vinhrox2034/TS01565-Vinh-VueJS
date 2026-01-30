df6f7ff7c20e12da54b0a6bd50547c39d4598de9
Vinhrox2034 <vinhlcts01565@gmail.com>
2026-01-30 23:46:23 +0700

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
-<script setup>
-import Hello from './components/HelloWorld.vue'
-import Bai3 from './components/Bai3.vue'
-import Bai4 from './components/Bai4.vue'
-</script>
-
 <template>
-  <Hello />
-  <hr />
-  <Bai3 />
-  <hr />
-  <Bai4 />
+  <div class="container mt-4">
+    <h2 class="text-center text-primary">LAB 5</h2>
+
+    <Bai1 />
+    <Bai2 />
+    <Bai3 />
+
+    <hr>
+    <CreatePost @add="posts.push($event)" />
+    <PostList :posts="posts" />
+  </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import Bai1 from './views/Bai1.vue'
+import Bai2 from './views/Bai2.vue'
+import Bai3 from './views/Bai3.vue'
+import CreatePost from './components/CreatePost.vue'
+import PostList from './components/PostList.vue'
+
+const posts = ref([])
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 createApp(App).mount('#app')
